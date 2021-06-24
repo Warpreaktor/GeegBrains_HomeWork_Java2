39789a418867c6ff010dd9b6d68e56a5a23efb94
warpreaktor <warpreaktor@gmail.com>
2021-06-24 09:34:35 +0300

    добавлено сообщение в MyArrayDataException о том, в какой ячейке массива ошибка с преобразованием к числу.

--- src/HomeWork_02/MyArrayDataException.java
+++ src/HomeWork_02/MyArrayDataException.java
@@ -1,8 +1,8 @@
 package HomeWork_02;
 
 public class MyArrayDataException extends Exception{
-    private static String message = "int не int. Давай до свидания!";
-    public MyArrayDataException() {
-        super(message);
+    private static String message = "int не int. Ошибка в ячейках: ";
+    public MyArrayDataException(int y, int x) {
+        super(message + y + " " + x);
     }
 }
